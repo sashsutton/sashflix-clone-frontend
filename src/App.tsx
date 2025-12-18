0cc813b3d3fb4596068e8bf88dfcfff641638ab5
sashasutton <sashasuttons3@gmail.com>
2025-12-18 22:20:22 +0100

    added the billboard on the app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Navbar from "./components/Navbar.tsx"
+import Billboard from "./components/Billboard.tsx";
 
 function App() {
 
   return (
       <div className="min-h-screen bg-black">
           <Navbar/>
+          <Billboard/>
+          <div className="h-screen bg-zinc-900"></div>
       </div>
+
   )
 }
 
