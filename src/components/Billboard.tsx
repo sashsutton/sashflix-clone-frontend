67d177ba876324deb1e0c983a20a8d50a334cb3f
sashasutton <sashasuttons3@gmail.com>
2025-12-18 22:32:42 +0100

    billboard file changed

--- src/components/Billboard.tsx
+++ src/components/Billboard.tsx
@@ -13,10 +13,19 @@ export default function Billboard(){
             <div className="absolute top-[30%] md:top-[40%] ml-4 md:ml-16">
                 <h1 className="text-white text-1xl md:text-5xl h-full w-[50%] lg:text-6xl font-bold drop-shadow-xl">Big Buck Bunny</h1>
                 <p className="text-white text-[8px] md:text-lg mt-3 md:mt-8 w-[90%] md:w-[80%] lg:w-[50%] drop-shadow-xl">A giant rabbit with a heart bigger than himself.</p>
-                <div>
-                    <button>Play</button>
-                    <button>More Info</button>
+
+                <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
+
+                    <button className="bg-white text-black py-1 md:py-2 px-2 md:px-4 w-auto text-xs lg:text-lg font-semibold flex flex-row items-center hover:bg-neutral-300 transition rounded-md">
+                        Play
+                    </button>
+
+                    <button className="bg-white/30 text-white py-1 md:py-2 px-2 md:px-4 w-auto text-xs lg:text-lg font-semibold flex flex-row items-center hover:bg-white/20 transition rounded-md">
+                        More Info
+                    </button>
+
                 </div>
+
             </div>
         </div>
     )
