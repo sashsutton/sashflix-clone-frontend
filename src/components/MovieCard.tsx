9e14bdc97f3501d99a5f8a6f0c444e5ec25c3f71
sashasutton <sashasuttons3@gmail.com>
2025-12-19 01:27:56 +0100

    pop-up effect

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -7,7 +7,47 @@ interface MovieCardProps{
 export default function MovieCard({data}: MovieCardProps){
     return(
         <div className="group bg-zinc-900 col-span relative h-[12vw]">
-            <img alt={data.title} src={data.thumbnailUrl} className="cursor-pointer object-cover transition duration shadow-xl rounded-md group-hover:opacity-90 sm:group-hover:opacity-0 delay-300 w-full h-[12vw]"/>
+
+            <img
+                alt={data.title}
+                src={data.thumbnailUrl}
+                className="cursor-pointer object-cover transition duration shadow-xl rounded-md w-full h-[12vw] group-hover:opacity-90 sm:group-hover:opacity-0 delay-300"
+            />
+
+            <div className="
+                opacity-0 absolute top-0 transition duration-200 z-10 invisible sm:visible delay-300 w-full
+                scale-0 group-hover:scale-110
+                group-hover:-translate-y-[6vw]
+                group-hover:translate-x-[2vw]
+                group-hover:opacity-100
+            ">
+                <img
+                    alt={data.title}
+                    src={data.thumbnailUrl}
+                    className="
+                        cursor-pointer object-cover transition duration shadow-xl rounded-t-md
+                        w-full h-[12vw]
+                    "
+                />
+                <div className="z-10 bg-zinc-800 p-2 lg:p-4 absolute w-full transition shadow-md rounded-b-md">
+
+                    <div className="flex flex-row items-center gap-3">
+                        <p className="text-white text-sm">Buttons will go here</p>
+                    </div>
+
+                    <div className="flex flex-row mt-4 gap-2 items-center">
+                        <p className="text-white text-[10px] lg:text-sm">{data.duration}</p>
+                    </div>
+
+                    <div className="flex flex-row mt-4 gap-2 items-center">
+                        <p className="text-white text-[10px] lg:text-sm">{data.genre}</p>
+                    </div>
+
+                </div>
+            </div>
+
+
+
         </div>
     )
 }
