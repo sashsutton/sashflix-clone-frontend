cd2ff4a8b566dac9e763e3489950852f8886a9a7
sashasutton <sashasuttons3@gmail.com>
2025-12-20 13:53:25 +0100

    extract movies data into a separate file

---
+++ src/data.ts
@@ -0,0 +1,34 @@
+export const movies = [
+    {
+        id: "1",
+        title: "Big Buck Bunny",
+        description: "Three rodents amuse themselves by harassing creatures of the forest. However, when they mess with a bunny, he decides to teach them a lesson.",
+        thumbnailUrl: "https://upload.wikimedia.org/wikipedia/commons/7/70/Big.Buck.Bunny.-.Opening.Screen.png",
+        genre: "Comedy",
+        duration: "10 mins"
+    },
+    {
+        id: "2",
+        title: "Sintel",
+        description: "A lonely young woman, Sintel, helps and befriends a dragon, whom she calls Scales. But when he is kidnapped by an adult dragon, Sintel decides to embark on a dangerous quest to find her lost friend Scales.",
+        thumbnailUrl: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/Sintel.jpg",
+        genre: "Adventure",
+        duration: "15 mins"
+    },
+    {
+        id: "3",
+        title: "Tears of Steel",
+        description: "In an apocalyptic future, a group of soldiers and scientists gathers at the Oude Kerk in Amsterdam to stage a crucial mission.",
+        thumbnailUrl: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/TearsOfSteel.jpg",
+        genre: "Sci-Fi",
+        duration: "12 mins"
+    },
+    {
+        id: "4",
+        title: "Elephants Dream",
+        description: "Friends Proog and Emo journey inside the folds of a massive, infinite machine.",
+        thumbnailUrl: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ElephantsDream.jpg",
+        genre: "Sci-Fi",
+        duration: "11 mins"
+    }
+];
